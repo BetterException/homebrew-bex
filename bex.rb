c0a62470e6b6dca2c397541650a6e6a6b6b70910
GhostVaibhav <48472541+GhostVaibhav@users.noreply.github.com>
2025-01-02 19:03:37 +0000

    Release: 0.2

--- bex.rb
+++ bex.rb
@@ -1,8 +1,8 @@
 class Bex < Formula
   desc "Better Exception handling"
   homepage "https://github.com/BetterException/bex"
-  url "https://github.com/BetterException/bex/archive/refs/tags/0.1.tar.gz"
-  sha256 "8890e660fbdf2fe77a7cee89f84872aaaa71d23629e01b9dc498494e109338a9"
+url "https://github.com/BetterException/bex/archive/refs/tags/0.2.tar.gz"
+sha256 "$HASH"
   depends_on "cmake" => :build
   depends_on "openssl" => :build
   def install
